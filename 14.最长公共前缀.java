c686cdae61e6476930afe79824e361dc9547fb66
zhutao <693117552@qq.com>
2019-04-10 19:20:17 +0800

    14

--- 14.最长公共前缀.java
+++ 14.最长公共前缀.java
@@ -36,13 +36,14 @@
  */
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        if (strs.length == 0) return "";
+        if (strs.length == 0 ) return "";
         String prefix = strs[0];
-        for (int i = 1; i < strs.length; i++){
-            while (strs[i].indexOf(prefix) != 0) {
-                prefix = prefix.substring(0, prefix.length() - 1);
+        int len = strs.length;
+        for (int i = 0; i < len; i++) {
+            while(strs[i].indexOf(prefix) != 0){
+                prefix = prefix.substring(0, prefix.length() -1);
                 if (prefix.isEmpty()) return "";
-            }   
+            }
         }
 
         return prefix;
