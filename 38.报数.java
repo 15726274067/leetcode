9ecef7aa2a004c99510e4cd7b85d9ad140ec9467
zhutao <693117552@qq.com>
2019-04-12 19:24:14 +0800

    countAndSay

---
+++ 38.报数.java
@@ -0,0 +1,78 @@
+import java.util.HashMap;
+
+/*
+ * @lc app=leetcode.cn id=38 lang=java
+ *
+ * [38] 报数
+ *
+ * https://leetcode-cn.com/problems/count-and-say/description/
+ *
+ * algorithms
+ * Easy (48.97%)
+ * Total Accepted:    26.8K
+ * Total Submissions: 54.6K
+ * Testcase Example:  '1'
+ *
+ * 报数序列是一个整数序列，按照其中的整数的顺序进行报数，得到下一个数。其前五项如下：
+ * 
+ * 1.     1
+ * 2.     11
+ * 3.     21
+ * 4.     1211
+ * 5.     111221
+ * 
+ * 
+ * 1 被读作  "one 1"  ("一个一") , 即 11。
+ * 11 被读作 "two 1s" ("两个一"）, 即 21。
+ * 21 被读作 "one 2",  "one 1" （"一个二" ,  "一个一") , 即 1211。
+ * 
+ * 给定一个正整数 n（1 ≤ n ≤ 30），输出报数序列的第 n 项。
+ * 
+ * 注意：整数顺序将表示为一个字符串。
+ * 
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 输入: 1
+ * 输出: "1"
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: 4
+ * 输出: "1211"
+ * 
+ * 1211 -> 111221 -> 312211 -> 13112221 -> 1113213211
+ * 
+ * 1211     
+ * i    curr   cout ret
+ * 0    1      1    11
+ * 1    2       1   12
+ * 2    1       2   21
+ */
+class Solution {
+    public String countAndSay(int n) {
+        HashMap<Integer, String> map = new HashMap<Integer, String>(n);
+        map.put(1, "1");
+        for (int i = 1; i < n; i++) {
+          map.put(i + 1, gen(map.get(i)));
+        }
+        return map.get(n);
+    }
+
+    public String gen(String str){
+        StringBuffer ret = new StringBuffer();
+        int i = 0, count = 1;
+
+        while (i < str.length()) {
+            String curr = str.substring(i, i+1);
+            while (i<str.length()-1 && curr.equals(str.substring(++i, i+1)) ) {
+                count++;
+            }
+            ret.append(count).append(curr);
+        }
+        return ret.toString();
+    }
+}
+
