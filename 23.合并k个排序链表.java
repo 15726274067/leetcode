0c2a381aa063ea2e26dd23ca996c797d29400607
zhutao2766 <zhutao@dding.cn>
2020-02-04 14:29:51 +0800

    23

---
+++ 23.合并k个排序链表.java
@@ -0,0 +1,66 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/*
+ * @lc app=leetcode.cn id=23 lang=java
+ *
+ * [23] 合并K个排序链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    /**
+     * 用容量为K的最小堆优先队列，把链表的头结点都放进去，
+     * 然后出队当前优先队列中最小的，挂上链表
+     * 然后让出队的那个节点的下一个入队，再出队当前优先队列中最小的，直到优先队列为空。
+     * @param lists
+     * @return
+     */
+    public ListNode mergeKLists(ListNode[] lists){
+        if(lists.length == 0)
+            return null;
+        if(lists.length == 1)
+            return lists[0];
+        if(lists.length == 2){
+           return mergeTwoLists(lists[0],lists[1]);
+        }
+
+        int mid = lists.length/2;
+        ListNode[] l1 = new ListNode[mid];
+        for(int i = 0; i < mid; i++){
+            l1[i] = lists[i];
+        }
+
+        ListNode[] l2 = new ListNode[lists.length-mid];
+        for(int i = mid,j=0; i < lists.length; i++,j++){
+            l2[j] = lists[i];
+        }
+
+        return mergeTwoLists(mergeKLists(l1),mergeKLists(l2));
+
+    }
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+
+        ListNode head = null;
+        if (l1.val <= l2.val){
+            head = l1;
+            head.next = mergeTwoLists(l1.next, l2);
+        } else {
+            head = l2;
+            head.next = mergeTwoLists(l1, l2.next);
+        }
+        return head;
+    }
+}
+// @lc code=end
+
